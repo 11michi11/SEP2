a33fdb0044d5129338f97694db2cfe9fe574f469
Unknown <michaela.golhova@gmail.com>
2018-04-10 08:26:09 +0200

    css file

--- SEP2/eNTe/src/client/view/ClientViewManager.java
+++ SEP2/eNTe/src/client/view/ClientViewManager.java
@@ -46,7 +46,7 @@ public class ClientViewManager extends Application implements ClientView {
 			loader = new FXMLLoader(getClass().getResource("/client/view/loginPane.fxml"));
 			Pane root = (Pane) loader.load();
 			Scene scene = new Scene(root, 1280, 780);
-			// scene.getStylesheets().add(getClass().getResource("client/view/login.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("/client/view/login.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 
@@ -69,7 +69,7 @@ public class ClientViewManager extends Application implements ClientView {
 			parentHandler = new ParentMainHandler();
 			loader.setController(parentHandler);
 			//Scene menuPane = new Scene(mainPane);
-			
+			mainPane.getStylesheets().add(getClass().getResource("/client/view/login.css").toExternalForm());
 			stage.getScene().setRoot(mainPane);;
 			stage.show();
 			
