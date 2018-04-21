c9c5c0186c2bf372467f1060b72b64fdbee441e3
DaniKoch1 <baqueta.dani@gmail.com>
2018-04-21 08:20:24 +0200

    Beginning of family in the class diagram and code

--- SEP2/eNTe/src/model/Parent.java
+++ SEP2/eNTe/src/model/Parent.java
@@ -4,45 +4,18 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Parent extends User implements Serializable {
-	private ArrayList<Student> children;
-	private String childrenNames;
-	private ArrayList<Class> classes;
 
-	public Parent(String name, String login, String pwd, ArrayList<Student> children) {
+	private ArrayList<Class> classes;
+	private Family family;
+
+	public Parent(String name, String login, String pwd, Family family) {
 		super(name, login, pwd);
-		initializeChildren(children);
+		this.family=family;
 	}
 	
-	public Parent(String name, String login, String pwd, ArrayList<Student> children, String id) {
+	public Parent(String name, String login, String pwd, Family family, String id) {
 		super(name, login, pwd, id);
-		initializeChildren(children);
+		this.family=family;
 	}
 
-	private void initializeChildren(ArrayList<Student> children) {
-		this.children = children;
-		classes = new ArrayList<Class>();
-		for (Student s : children)
-			classes.add(s.getClasss());
-
-		childrenNames = "";
-		for (Student s : children)
-			childrenNames += s.getName() + ", ";
-	}
-
-	public String getChildrenNames() {
-		return childrenNames;
-	}
-
-	public void addChild(Student child) {
-		children.add(child);
-		classes.add(child.getClasss());
-	}
-
-	public void removeChild(Student child) {
-		children.remove(child);
-	}
-
-	public ArrayList<Student> getChildren() {
-		return children;
-	}
 }
