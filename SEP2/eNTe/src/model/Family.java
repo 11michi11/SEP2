698e392b230601bfa9e3b0eefc10a57455d5db06
11michi11 <11michi11@gmail.com>
2018-05-09 15:49:56 +0200

    sending mail utility

--- SEP2/eNTe/src/model/Family.java
+++ SEP2/eNTe/src/model/Family.java
@@ -3,65 +3,71 @@ package model;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
-public class Family{
-	private String id;
-	private ArrayList<Student> children;
-	private ArrayList<Parent> parents;
-	
-	public Family() {
-		id = UUID.randomUUID().toString();
-		children= new ArrayList<>();
-		parents= new ArrayList<>();
-	}
-	public void addChild(Student child) {
-		children.add(child);
-	}
+public class Family {
+    private String id;
+    private ArrayList<Student> children = new ArrayList<>();
+    private ArrayList<Parent> parents = new ArrayList<>();
 
-	public void deleteChild(Student child) {
-		children.remove(child);
-	}
+    public Family() {
+        id = UUID.randomUUID().toString();
+    }
 
-	public ArrayList<Student> getChildren() {
-		return children;
-	}
+    public void addChild(Student child) {
+        children.add(child);
+    }
 
-	public Student getChild(String name) throws NoSuchElementException {
-		for (Student e : children)
-			if (e.getName().equals(name))
-				return e;
-		throw new NoSuchElementException(name + "was not found.");
-	}
-	
-	public Parent getParent(String name) throws NoSuchElementException {
-		for (Parent e : parents)
-			if (e.getName().equals(name))
-				return e;
-		throw new NoSuchElementException(name + "was not found.");
-	}
+    public void deleteChild(Student child) {
+        children.remove(child);
+    }
 
-	public void addParent(Parent parent) {
-		parents.add(parent);
-	}
+    public ArrayList<Student> getChildren() {
+        return children;
+    }
 
-	public void deleteParent(Parent parent) {
-		parents.remove(parent);
-	}
+    public Student getChild(String name) throws NoSuchElementException {
+        for (Student e : children)
+            if (e.getName().equals(name))
+                return e;
+        throw new NoSuchElementException(name + "was not found.");
+    }
 
-	public ArrayList<Parent> getParents() {
-		return parents;
-	}
+    public Parent getParent(String name) throws NoSuchElementException {
+        for (Parent e : parents)
+            if (e.getName().equals(name))
+                return e;
+        throw new NoSuchElementException(name + "was not found.");
+    }
 
-	public ArrayList<Classs> getClasses() {
-		ArrayList<Classs> classes = new ArrayList<>();
-		for (Student e : children) {
-		    if(classes.contains(e.getClasss()))
-				classes.add(e.getClasss());
-		}
-		return classes;
-	}
+    public String getMembersNames() {
+        String parentsNames = parents.stream().map(s -> s.getName() + ", ").collect(Collectors.joining());
+        String childrenNames = children.stream().map(s -> s.getName() + ", ").collect(Collectors.joining());
+        return parentsNames + childrenNames.substring(0, childrenNames.length() - 2);
+    }
+
+    public void addParent(Parent parent) {
+        parents.add(parent);
+    }
+
+    public void deleteParent(Parent parent) {
+        parents.remove(parent);
+    }
+
+    public ArrayList<Parent> getParents() {
+        return parents;
+    }
+
+    public ArrayList<Classs> getClasses() {
+        ArrayList<Classs> classes = new ArrayList<>();
+        for (Student e : children) {
+            if (classes.contains(e.getClasss()))
+                classes.add(e.getClasss());
+        }
+        return classes;
+    }
 
     public String getId() {
         return id;
-	}
+    }
 }
