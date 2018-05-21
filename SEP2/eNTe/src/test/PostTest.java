c4a233c240507f664f2c69af9657d4caf743249f
Miska <michaela.golhova@gmail.com>
2018-05-21 15:16:51 +0200

    post test

--- SEP2/eNTe/src/test/PostTest.java
+++ SEP2/eNTe/src/test/PostTest.java
@@ -2,6 +2,7 @@ package test;
 
 import model.MyDate;
 import model.Post;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static junit.framework.Assert.assertEquals;
@@ -9,11 +10,11 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class PostTest {
 
-	@Test
-	void testGetContent() {
-		MyDate pubDate = MyDate.now();
-		Post post = new Post("title", "content", "Author", pubDate);
-		assertEquals("content", post.getContent());
+	private MyDate date;
+
+	@BeforeEach
+	void setUp() {
+		MyDate date = new MyDate(2018, 8, 15);
 	}
 	
 	@Test
@@ -32,6 +33,47 @@ class PostTest {
 		assertNotEquals(post1, post4);
 		assertNotEquals(post4, post3);
 		assertNotEquals(post1, new Object());
+	}
+
+	@Test
+	void constructorTest() {
+		Post post = new Post("id","title", "content", "author", date);
+		assertEquals("id", post.getPostId());
+		assertEquals("title", post.getTitle());
+		assertEquals("content", post.getContent());
+		assertEquals("author", post.getAuthor());
+		assertEquals(date, post.getPubDate());
+	}
+
+	@Test
+	void getTitleTest() {
+		Post post = new Post("title", "content", "author", date);
+		assertEquals("title", post.getTitle());
+	}
+
+	@Test
+	void getContentTest() {
+		Post post = new Post("title", "content", "author", date);
+		assertEquals("content", post.getContent());
+	}
+
+	@Test
+	void getAuthorTest() {
+		Post post = new Post("title", "content", "author", date);
+		assertEquals("author", post.getAuthor());
+	}
+
+	@Test
+	void getPubDateTest() {
+		Post post = new Post("title", "content", "author", date);
+		assertEquals(date, post.getPubDate());
+	}
+
+	@Test
+	void postIdTest() {
+		Post post = new Post("id","title", "content", "author", date);
+		assertEquals("id", post.getPostId());
+
 	}
 
 }
