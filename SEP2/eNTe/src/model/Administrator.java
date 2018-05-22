676f26215b76ac6e077b8f2d9b58df77c70d6ed2
Miska <michaela.golhova@gmail.com>
2018-05-22 17:39:55 +0200

    Administrator builder

--- SEP2/eNTe/src/model/Administrator.java
+++ SEP2/eNTe/src/model/Administrator.java
@@ -21,5 +21,79 @@ public class Administrator extends User implements Serializable {
         return super.toString();
     }
 
+    public static AdminNeedName builder() {
+        return new Builder();
+    }
 
+    public static final class Builder implements AdminNeedName, AdminNeedEmail, AdminCanBeBuild {
+
+        protected String id;
+        private String name;
+        private String email;
+        private String pwd;
+        private boolean encryptPwd;
+
+
+        @Override
+        public AdminNeedEmail name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        @Override
+        public AdminCanBeBuild email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        @Override
+        public AdminCanBeBuild pwd(String pwd) {
+            this.pwd = pwd;
+            return this;
+        }
+
+        @Override
+        public AdminCanBeBuild pwdEncrypt(String pwd) {
+            this.pwd = pwd;
+            encryptPwd = true;
+            return this;
+        }
+
+        @Override
+        public AdminCanBeBuild id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        @Override
+        public Administrator build() {
+            Administrator administrator = new Administrator(this.name, this.email);
+            if (this.pwd != null)
+                if (encryptPwd)
+                    administrator.setPwd(pwd);
+                else
+                    administrator.setPwdNoEncrypt(pwd);
+            administrator.id = this.id;
+            return administrator;
+        }
+    }
+
+    public interface AdminNeedName {
+        AdminNeedEmail name(String name);
+    }
+
+    public interface AdminNeedEmail {
+        AdminCanBeBuild email(String name);
+    }
+
+    public interface AdminCanBeBuild {
+        Administrator build();
+
+        AdminCanBeBuild pwd(String pwd);
+
+        AdminCanBeBuild pwdEncrypt(String pwd);
+
+        AdminCanBeBuild id(String id);
+    }
+    
 }
