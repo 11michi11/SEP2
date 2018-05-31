203c8f9cea6b8eba1839a5f8c1e6ceb212368eb7
Miska <michaela.golhova@gmail.com>
2018-05-31 17:05:05 +0200

    smth

--- SEP2/eNTe/src/client/view/ParentMainHandler.java
+++ SEP2/eNTe/src/client/view/ParentMainHandler.java
@@ -2,12 +2,16 @@ package client.view;
 
 import client.controller.ClientController;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.scene.text.TextFlow;
+import model.Homework;
 import model.Post;
+
+import java.util.ArrayList;
 
 public class ParentMainHandler {
 
@@ -25,20 +29,30 @@ public class ParentMainHandler {
 	public void initialize() {
 		System.out.println("second");
 		System.out.println(box);
+		ArrayList<Post> posts = controller.getAllPosts();
+		for(Post p : posts) {
+			loadPost(p);
+		}
 
-		Post[] posts = controller.getPosts();
+	}
 
-		Text title = new Text(posts[0].getTitle());
+	public void loadPanes(Pane pane) {
+		box.getChildren().add(pane);
+	}
+	
+
+	private void loadPost(Post post) {
+		Text title = new Text(post.getTitle());
 		title.setId("title");
-		Text content = new Text(posts[0].getContent());
+		Text content = new Text(post.getContent());
 		content.setId("content");
-		Text separator = new Text("\n");
+		Text separator = new Text("\n" + "\n");
 
-		TextFlow textFlow = new TextFlow(title, separator,  content);
+		TextFlow textFlow = new TextFlow(title, separator, content);
 		textFlow.setTextAlignment(TextAlignment.JUSTIFY);
-		textFlow.setAccessibleText(posts[0].getContent());
+		textFlow.setAccessibleText(post.getContent());
 		textFlow.setPrefWidth(842);
-		
+
 		Pane pane = new Pane() {
 			@Override
 			protected void layoutChildren() {
@@ -51,12 +65,6 @@ public class ParentMainHandler {
 		pane.getChildren().add(textFlow);
 		pane.getStyleClass().add("textPane");
 		loadPanes(pane);
-
-	}
-
-	public void loadPanes(Pane pane) {
-		box.getChildren().clear();
-		box.getChildren().add(pane);
 	}
 
 }
--- SEP2/eNTe/src/client/view/TeacherMainHandler.java
+++ SEP2/eNTe/src/client/view/TeacherMainHandler.java
@@ -2,13 +2,17 @@ package client.view;
 
 import client.controller.ClientController;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
+import model.Homework;
 import model.Post;
+
+import java.util.ArrayList;
 
 public class TeacherMainHandler {
 	@FXML
@@ -27,18 +31,43 @@ public class TeacherMainHandler {
 	public void initialize() {
 		System.out.println("second");
 		System.out.println(box);
+		ArrayList<Post> posts = controller.getAllPosts();
+		for(Post p : posts) {
+			switch(p.getClass().getSimpleName()) {
+				case "Homework":
+					loadHomework((Homework) p);
+					break;
+				case "Post":
+					loadPost(p);
+					break;
+				default:
+					break;
+			}
+		}
 
-		Post[] posts = controller.getPosts();
+	}
 
-		Text title = new Text(posts[0].getTitle());
+	public void loadPanes(Pane pane) {
+		box.getChildren().add(pane);
+	}
+	private void loadHomework(Homework homework) {
+
+		Text title = new Text(homework.getTitle());
 		title.setId("title");
-		Text content = new Text(posts[0].getContent());
+		Text content = new Text(homework.getContent());
 		content.setId("content");
+		Text deadline = new Text(homework.getDeadline().toString());
 		Text separator = new Text("\n" + "\n");
+		Text separator1 = new Text("\n" + "\n" + " ");
+		Text separator2 = new Text("\n" + "\n" + " ");
 
-		TextFlow textFlow = new TextFlow(title, separator, content);
+		Button list = new Button("DONE BY:");
+		list.getStyleClass().add("smallButton");
+
+
+		TextFlow textFlow = new TextFlow(title, separator, content, separator1, deadline, separator2, list);
 		textFlow.setTextAlignment(TextAlignment.JUSTIFY);
-		textFlow.setAccessibleText(posts[0].getContent());
+		textFlow.setAccessibleText(homework.getContent());
 		textFlow.setPrefWidth(842);
 
 		Pane pane = new Pane() {
@@ -50,15 +79,34 @@ public class TeacherMainHandler {
 				autosize();
 			}
 		};
+		pane.getChildren().addAll(textFlow);
+		pane.getStyleClass().add("textPane");
+		loadPanes(pane);
+	}
+
+	private void loadPost(Post post) {
+		Text title = new Text(post.getTitle());
+		title.setId("title");
+		Text content = new Text(post.getContent());
+		content.setId("content");
+		Text separator = new Text("\n" + "\n");
+
+		TextFlow textFlow = new TextFlow(title, separator, content);
+		textFlow.setTextAlignment(TextAlignment.JUSTIFY);
+		textFlow.setAccessibleText(post.getContent());
+		textFlow.setPrefWidth(842);
+
+		Pane pane = new Pane() {
+			@Override
+			protected void layoutChildren() {
+				super.layoutChildren();
+				TextFlow textFlow = (TextFlow) getChildren().get(0);
+				setMinHeight(textFlow.getHeight()+5);
+				autosize();
+			}
+		};
 		pane.getChildren().add(textFlow);
 		pane.getStyleClass().add("textPane");
 		loadPanes(pane);
-
-	}
-
-
-	public void loadPanes(Pane pane) {
-		box.getChildren().clear();
-		box.getChildren().add(pane);
 	}
 }
